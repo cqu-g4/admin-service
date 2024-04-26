d402d1627cec8f5faad3e197786a028be80e4ea8
Sangharsha Chaulagain <sangharsha.chaulagain@gmail.com>
2024-04-26 13:30:57 +1000

    Initial Commit

---
+++ src/main/java/au/edu/cqu/g4/adminservice/AdminServiceApplication.java
@@ -0,0 +1,13 @@
+package au.edu.cqu.g4.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class, args);
+    }
+
+}
