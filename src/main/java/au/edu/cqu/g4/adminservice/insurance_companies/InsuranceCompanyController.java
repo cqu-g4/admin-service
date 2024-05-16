653cc2f938bc63b71ca102f7e694a2e50d23bba6
Sangharsha Chaulagain <sangharsha.chaulagain@gmail.com>
2024-05-16 14:19:15 +1000

    change the url of the insurance company api

--- src/main/java/au/edu/cqu/g4/adminservice/insurance_companies/InsuranceCompanyController.java
+++ src/main/java/au/edu/cqu/g4/adminservice/insurance_companies/InsuranceCompanyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/insurance_companies")
+@RequestMapping("/insurance_companies")
 @RequiredArgsConstructor
 public class InsuranceCompanyController {
 
